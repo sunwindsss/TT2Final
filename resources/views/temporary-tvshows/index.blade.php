32c3e2b23f5cca6456bd41fbbb12cfa6ec86e226
sunwinds <55029946+sunwindsss@users.noreply.github.com>
2023-06-16 22:43:13 +0300

    Added Laravel Breeze Authentication functionality, changed the default welcome view to be the default TV show list view, added Login/Register buttons to the TV show view, and also added a Back to Tracker link when logging in.

--- resources/views/temporary-tvshows/index.blade.php
+++ resources/views/temporary-tvshows/index.blade.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Temporary TV Shows</title>
+    <title>TV Trekeris!</title>
     <link rel="stylesheet" href="{{ asset('css/temp-styles.css') }}">
+    <style>
+        .top-right {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+
+        .top-right a {
+            background-color: yellow;
+            color: black;
+            padding: 5px 10px;
+            border-radius: 5px;
+            margin-left: 5px;
+        }
+
+        .top-right form {
+            display: inline;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
-        <h1>All Temporary TV Shows</h1>
+        <div class="top-right">
+            @if (Auth::check())
+                <!-- Show logout button -->
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            @else
+                <!-- Show login and register buttons -->
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+        </div>
+        <h1>TV Show Tracker</h1>
         <div class="tv-show-list">
             @foreach ($temporaryTVShows as $temporaryTVShow)
                 <div class="tv-show">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TemporaryTVShowController;
 
@@ -14,5 +15,24 @@ use App\Http\Controllers\TemporaryTVShowController;
 |
 */
 
-// Route for displaying temporary TV shows
+/* Default welcome Laravel view (with login/register)
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Route::get('/', [TemporaryTVShowController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 Route::get('/temporary-tvshows', [TemporaryTVShowController::class, 'index'])->name('temporary-tvshows.index');
