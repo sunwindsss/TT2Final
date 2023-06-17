43511c88c9fb9b0c346c9bd00284bf7bd9effae7
sunwinds <55029946+sunwindsss@users.noreply.github.com>
2023-06-18 00:53:03 +0300

    Updated main page rating design.
    Main page rating design has been modified, the message for unregistered users is much more clean, the "remove rating" button stands out a lot more, and the main page title has been modified to be bigger and use a different font. Other small design changes for the main page.

--- resources/views/temporary-tvshows/index.blade.php
+++ resources/views/temporary-tvshows/index.blade.php
@@ -21,6 +21,39 @@
         .top-right form {
             display: inline;
         }
+
+        h1 {
+            font-family: "Inter", sans-serif;
+            font-size: 40px;
+            margin-top: 20px;
+        }
+
+        /* border-bottom creates the white line effect */ 
+        .tv-show {
+            border-bottom: 1px solid #ccc;
+            padding-bottom: 20px;
+            margin-bottom: 20px;
+        }
+
+        .rating-info {
+            display: flex;
+            align-items: center;
+        }
+
+        .rating-info .score {
+            margin-bottom: 0;
+            margin-right: 10px;
+        }
+
+        /* Delete button is styled with a red background color, to make it stand out */
+        .delete-rating {
+            background-color: red;
+            color: white;
+            border: none;
+            padding: 3px 8px;
+            border-radius: 3px;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
@@ -41,7 +74,7 @@
                 <a href="{{ route('register') }}">Register</a>
             @endif
         </div>
-        <h1>TV Show Tracker</h1>
+        <h1 class="title">TV Show Tracker</h1>
         <!-- Code for listing each TV Show and Actors, and Ratings -->
         <div class="tv-show-list">
             @foreach ($temporaryTVShows as $temporaryTVShow)
@@ -65,7 +98,7 @@
                                         <form action="{{ route('tvshows.rate.delete', ['rating' => $userRating->id]) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="delete-rating">X</button>
+                                            <button type="submit" class="delete-rating" style="margin-top: 15px;"><strong>X</strong></button>
                                         </form>
                                     </div>
                                 @else
@@ -73,14 +106,14 @@
                                         @csrf
                                         <div class="form-group">
                                             <label for="rating">Rate this show:</label>
-                                            <input type="number" name="rating" id="rating" min="1" max="10" step="1" required>
+                                            <input type="number" name="rating" id="rating" min="1" max="10" step="1" required style="width: 35px;">
                                             <input type="hidden" name="tv_show_id" value="{{ $temporaryTVShow->id }}">
+                                            <button type="submit">Rate</button>
                                         </div>
-                                        <button type="submit">Rate</button>
                                     </form>
                                 @endif
                                 @else
-                                    <p>Please <a href="{{ route('register') }}">register</a> to rate shows.</p>
+                                    <p>Please <a href="{{ route('register') }}" style="color: gold; text-decoration: underline;">login</a> to rate shows.</p>
                             @endauth
                         </div>
                 </div>
